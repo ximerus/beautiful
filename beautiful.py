1c392c04da49db91d765f9e45eeff8c6df1633a4
ximerus <ximerus@gmail.com>
2015-04-02 13:20:36 +0300

    first commit

---
+++ beautiful.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main():
+    print "Программа вычисления коэфициента женской красоты:"
+    print "-------------------------------------------------"
+    breast = int(input("Введите обхват бюста:"))
+    hips = int(input("Введите обхват по бедрам:"))
+    waist = int(input("Введите обхват по талии:"))
+    growth = int(input("Введите рост:"))
+    weight = int(input("Введите вес:"))
+    print"--------------------------------------------------"
+    print"Индекс женской красоты равен:"
+    print beauti(breast, hips, waist, growth, weight)
+
+
+def beauti(breast, hips, waist, growth, weight):
+    x = breast * hips * growth
+    y = (waist**2) * weight
+    return x/y
+
+if __name__ == "__main__":
+        main()
